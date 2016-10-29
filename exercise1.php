3b2e75c66912615f798d37e1eaf501231668c151
Jersson Bacilio <jerss.edu@gmail.com>
2016-10-29 07:25:36 -0500

    Ejercicio 1

---
+++ exercise1.php
@@ -0,0 +1,11 @@
+<h1>Ejercicio 1</h1>
+<?php
+    //Variable de tipo cadena
+    $uno="Contiene una variable 1";
+    //Variable de tipo integer
+    $dos=128;
+    
+    echo "Uno: ".$uno."<br>";
+    echo "Dos: ".$dos."<hr>";
+?>
+
