4aea2281ef71991869a37be784b0f803302d76ee
Jersson Bacilio <jerss.edu@gmail.com>
2016-10-29 07:44:13 -0500

    Verificar si un número es par o impar

---
+++ exercise3.php
@@ -0,0 +1,17 @@
+<?php
+    for($i=1; $i<=30 ; $i++){
+        
+        $cuadrado = $i*$i;
+        echo "El cuadrado de ". $i. " es :". $cuadrado;
+        
+        if (($cuadrado) % 2 ==0)
+            echo " y es par";
+        else
+            echo " y es IMPAR";
+            
+        echo "<br>";
+    }
+?>
+
+
+
